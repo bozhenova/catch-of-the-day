53bd02b0872cb5884eb5b42fdb1509f6cc12c134
Margo <margo.bozhenova@gmail.com>
2020-06-15 04:34:00 +0300

    Add Fish and Inventory classes

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import AddFishForm from '../AddFishForm/AddFishForm';
+import EditFishForm from '../EditFishForm/EditFishForm';
 
 class Inventory extends Component {
   render() {
-    return <div className='inventory'></div>;
+    const {
+      fishes,
+      addFish,
+      loadSampleFishes,
+      updateFish,
+      deleteFish
+    } = this.props;
+    return (
+      <div className='inventory'>
+        <h2>Inventory</h2>
+        {Object.keys(fishes).map(key => (
+          <EditFishForm
+            key={key}
+            index={key}
+            fish={fishes[key]}
+            updateFish={updateFish}
+            deleteFish={deleteFish}
+          />
+        ))}
+        <AddFishForm addFish={addFish} />
+        <button onClick={loadSampleFishes}>Load Sample Fishes</button>
+      </div>
+    );
   }
 }
 
